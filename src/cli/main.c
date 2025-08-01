c162087b325e8bd1a0490dad080733a4d29947e7
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-01 05:51:47 -0300

    starting the cli

--- src/cli/main.c
+++ src/cli/main.c
@@ -4,11 +4,7 @@
 //silver_chain_scope_end
 
 int main(){
-    char *content = dtw_load_string_file_content("exemple.c");
-    char *output = mdeclare_transform_content(content);
-    dtw_write_string_file_content("output.c", output);
-    free(content);
-    free(output);
+
     return 0;
 
 }
